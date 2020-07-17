d1b78e3b25e43d0c2cbbf83ec42fcb221cdacb3b
TinhVV <tinhvv@ominext.com>
2020-07-17 11:07:08 +0700

    welcome to my project

---
+++ TestCI/TestCI/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TestCI
+//
+//  Created by tinhvv-ominext on 7/17/20.
+//  Copyright © 2020 tinhvv-ominext. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TestCI/TestCI/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TestCI
+//
+//  Created by tinhvv-ominext on 7/17/20.
+//  Copyright © 2020 tinhvv-ominext. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
